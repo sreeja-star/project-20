d2209c0b972ca9c66736958051d4f3be83860ff2
sreeja-star <72139351+sreeja-star@users.noreply.github.com>
2020-10-20 18:49:37 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,22 @@
+const Engine=Matter.Engine;
+const World=Matter.World;
+const Matter=Matter.Bodies;
+
+var engine, world;
+var object;
+
+function setup() {
+  createCanvas(400,400);
+  engine=Engine.create();
+  world =engine.world;
+
+  object=Bodies.rectangle(200,100,50,50);
+  World.add(world.object);
+ console.log(object);
+}
+
+function draw() {
+  background("black");  
+  rectMode(CENTER);
+   rect(200,200,50,50);
+}
